e1c4fdea5252428057eae03b18554822156e6033
Varun Rao <varunraob@gmail.com>
2020-05-20 09:56:38 -0500

    dos2unix conversion

--- v3/scripts/install-ranger-agents.sh
+++ v3/scripts/install-ranger-agents.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-set -euo pipefail
-set -x
+#set -euo pipefail
+#set -x
+
 isMasterInstance=$(cat /mnt/var/lib/info/instance.json | jq '.isMaster')
 s3bucket=$1
 s3_key=$2
+ranger_host=$3
+ranger_version=$4
+target=$5
+
 if [ "${isMasterInstance}" == "true" ];
 then
     echo "Master instance. Setting up..."
-    sudo sed 's/null &/null \&\& hadoop fs -get s3:\/\/aws-bigdata-blog\/artifacts\/aws-blog-emr-ranger-v3\/scripts\/emr-steps\/install-hive-hdfs-ranger-plugin.sh \&\& hadoop fs -get s3:\/\/aws-bigdata-blog\/artifacts\/aws-blog-emr-ranger-v3\/scripts\/emr-steps\/install-hive-hdfs-ranger-policies.sh \&\& bash install-hive-hdfs-ranger-plugin.sh >> $STDOUT_LOG 2>> $STDERR_LOG \&\& bash install-hive-hdfs-ranger-policies.sh >> $STDOUT_LOG 2>> $STDERR_LOG \&\n/g' /usr/share/aws/emr/node-provisioner/bin/provision-node > ~/provision-node.new
-    sudo cat ~/provision-node.new
+    sudo sed "s|null \&|null \&\& hadoop fs -get s3://${s3bucket}/${s3_key}/scripts/emr-steps/install-hive-hdfs-ranger-plugin.sh \&\& hadoop fs -get s3://${s3bucket}/${s3_key}/scripts/emr-steps/install-hive-hdfs-ranger-policies.sh \&\& sed -i 's/\r//' install-hive-hdfs-ranger-plugin.sh \&\& bash install-hive-hdfs-ranger-plugin.sh $ranger_host $ranger_version $target \&\& bash install-hive-hdfs-ranger-policies.sh \&\n|g" /usr/share/aws/emr/node-provisioner/bin/provision-node > ~/provision-node.new
     sudo cp ~/provision-node.new /usr/share/aws/emr/node-provisioner/bin/provision-node
+#    echo "alias restart='sudo stop hadoop-hdfs-namenode && sudo start hadoop-hdfs-namenode && sudo stop hive-server2 && sudo start hive-server2'" >> ~/.bash_profile
 else
-    echo "Slave instance. Doing nothing..."
+echo "Slave instance. Doing nothing..."
 fi
